206dbfdb7c5c20bd978805baf5c114c44f8a8196
guillermomozolopez <guillermomozolopez@gmail.com>
2019-02-05 12:03:36 +0100

    enter

--- index.php
+++ index.php
@@ -4,25 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./style.css">
     <title>Document</title>
 </head>
 <body>
+<div class="centrar">
 <h1>Stopping-Pellas</h1>
-    <div>
         <h2>
             Log-in
         </h2>
         <p>
-            <table>
-                <tr>
-                    <td>Usuario</td>
-                    <td><input type="text" name="usuario" id=""></td>
-                </tr>
-                <tr>
-                    <td>Contraseña</td>
-                    <td><input type="password" name="contra" id=""></td>
-                </tr>
-            </table>
+            <form action="./validar.php" method="get">
+                <table>
+                    <tr>
+                        <td>Usuario</td>
+                        <td><input type="text" name="usuario" id=""></td>
+                    </tr>
+                    <tr>
+                        <td>Contraseña</td>
+                        <td><input type="password" name="contra" id=""></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" value="Aceptar"></td>
+                    </tr>
+                </table>
+            </form>
         </p>
     </div>
 </body>
