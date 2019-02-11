a1531f9ae3e32cc16becea15303d79010dffa0fe
sergiopuce93 <sergiomu.garcia@gmail.com>
2019-02-11 21:25:15 +0100

    cambios login

--- Login/index.php
+++ Login/index.php
@@ -1,18 +1,18 @@
+<?php
+    
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
     <link rel="stylesheet" href="../Estilos/login.css">
+    <title>Document</title>
 </head>
 <body>
-    <h1>Login</h1>
-    <form action="login.php" method="POST">
-    <input type="text" name="email" placeholder="Enter your mail">
-    <input type="password" name="password" placeholder="Enter you password">
-    <input type="submit" value="Send">
-    </form>
+    <h1>Please Login or SignUp</h1>
+    <a href="login.php">Login</a>
+    <a href="signup.php">Signup</a>
 </body>
 </html>
