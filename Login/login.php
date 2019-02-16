f06f144f9e160cc27a214ea38cd364647e482b0c
sergiopuce93 <sergiomu.garcia@gmail.com>
2019-02-16 14:45:23 +0100

    Update login.php

--- Login/login.php
+++ Login/login.php
@@ -1,3 +1,34 @@
+<?php
+$error = '';
+//Validacion envio de datos con campos vacios
+if (isset($_POST['botonEnviar'])) {
+    if (empty($_POST['dni']) && empty($_POST['password'])) {
+        $error = "Dni y contraseña vacios";
+    } else if (empty($_POST['dni'])) {
+        $error = "Dni vacio";
+    } else if (empty($_POST['password'])) {
+        $error = "Contraseña vacia";
+    } else {
+        //validacion envio de datos correcta
+        require "./clase_login.php";
+        $objetoLogin = new Login();
+        $existeUsuario = $objetoLogin->existeUsuario($_POST['dni'], $_POST['password']);
+        echo $existeUsuario;
+        //si existe el usuario
+        if ($existeUsuario) {
+            $dni = $_POST['dni'];
+
+        } else {
+            //si no existe redireccionamos a la misma pagina
+            header("Location:login.php");
+        }
+
+    }
+
+} else {
+    //validacion incorrect de envio de datos
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,18 +37,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../Estilos/login.css">
     <title>Login</title>
-    
+
 </head>
 <body>
     <?php
-    require "./headerLogin.php";
-    ?>
-    
+require "./headerLogin.php";
+?>
+    <div class="error">
+    <?php
+if (!empty($error)): ?>
+    <p><?=$error?></p>
+    <?php endif;?>
+    </div>
     <form action="login.php" method="POST">
-    <input type="text" name="email" placeholder="Introduzca Dni">
+    <input type="text" name="dni" placeholder="Introduzca Dni">
     <input type="password" name="password" placeholder="Introduzca contraseña">
-    <input type="submit" value="Send">
-    <span><a href="singup.php">Olvidaste la contraseña</a></span> 
+    <input type="submit" value="Send" name="botonEnviar">
+    <span><a href="singup.php">Olvidaste la contraseña</a></span>
     </form>
+
+
 </body>
 </html>
