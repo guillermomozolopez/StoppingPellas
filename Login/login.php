15bcbd0952cbac123a902f6bb89505eba9730d29
sergiopuce93 <sergiomu.garcia@gmail.com>
2019-02-16 12:06:41 +0100

    cambio login

---
+++ Login/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../Estilos/login.css">
+    <title>Login</title>
+    
+</head>
+<body>
+
+    <h1>Login</h1>
+    <span>or <a href="singup.php">SingUp</a></span>
+    <form action="login.php" method="POST">
+    <input type="text" name="email" placeholder="Enter your mail">
+    <input type="password" name="password" placeholder="Enter you password">
+    <input type="submit" value="Send">
+    </form>
+</body>
+</html>
