6d3ef3f982b843cd3c3649552fb31bffc8a1ab9f
sergiopuce93 <sergiomu.garcia@gmail.com>
2019-02-16 14:45:15 +0100

    Create clase_login.php

---
+++ Login/clase_login.php
@@ -0,0 +1,35 @@
+<?php
+require "../Config/conexion.php";
+class Login extends Conexion
+{
+
+    public function __contruct()
+    {
+        parent::__construct();
+
+    }
+    public function existeUsuario($dni,$pass)
+    {
+        $sql = "SELECT COUNT(*) as 'cont' FROM USUARIO WHERE DNI='$dni' and CONTRASEÑA='$pass';";
+        $resultado = $this->conexion_db->query($sql);
+        $existe = $resultado->fetch_all(MYSQLI_ASSOC);
+        if($existe[0]['cont'] > 0){
+        return true;
+        }
+        return false;
+        
+    }
+    public function esProfe()
+    {
+        
+    }
+    public function esAlumno()
+    {
+
+    }
+    public function esAdmin()
+    {
+
+    }
+
+}
