f0ab104240a961272f0e929aae01e313dc544b92
Guillermo Mozo Lopez <guillermomozolopez@gmail.com>
2019-02-17 23:36:56 +0100

    guardar

--- profesoresPHP/pantallaProfesores.php
+++ profesoresPHP/pantallaProfesores.php
@@ -15,6 +15,7 @@ $listaAsignaturas = $profesor->listarAsignaturas($_SESSION['user']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../Estilos/ProfesorDiseño.css">
     <title>Profesores</title>
     <style>
         tr, td {
@@ -23,6 +24,7 @@ $listaAsignaturas = $profesor->listarAsignaturas($_SESSION['user']);
     </style>
 </head>
 <body>
+<div class="fondo"></div>
 <!-- header -->
 
 <?php
