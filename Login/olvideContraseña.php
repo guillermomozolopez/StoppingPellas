f80f873f8b4025e5dea18a1d5d5a6f230f910f29
sergiopuce93 <sergiomu.garcia@gmail.com>
2019-02-17 23:15:17 +0100

    reposo diseño

--- Login/olvideContraseña.php
+++ Login/olvideContraseña.php
@@ -29,6 +29,7 @@ if (isset($_POST['enviar'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
     <title>Document</title>
 </head>
 <body>
