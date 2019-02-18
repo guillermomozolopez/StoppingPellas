1ba9a23e7ff090c42dbd41c3cb84f06b1268cd85
sergiopuce93 <sergiomu.garcia@gmail.com>
2019-02-18 17:30:43 +0100

    cookie eliminar cerrar sesion

--- Login/cerrarSesion.php
+++ Login/cerrarSesion.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 session_destroy();
-echo 'Cerraste sesión';
+echo 'Cerrasndo sesión...';
+unset($_COOKIE['dni']);
 echo '<script> window.location="./login.php"; </script>';
+
 ?>
