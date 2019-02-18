75e763e763b20a78a9024596567356c8b70a78df
Guillermo Mozo Lopez <guillermomozolopez@gmail.com>
2019-02-18 20:19:25 +0100

    ya no se que poner

--- Login/headerLogin.php
+++ Login/headerLogin.php
@@ -4,7 +4,7 @@ echo "<div id='header'>";
     echo "<div id='logo'></div>";
     echo "<div>";
         echo "<a href='../miPerfilPHP/pantallaMiPerfil.php'><i class='fas fa-user'></i></a>";
-        echo "<a href='../Login/cerrarSesion.php'><i class='fas fa-window-close'></i></a>";
+        echo "<a href='../Login/cerrarSesion.php'><i class='fas fa-power-off'></i></a>";
     echo "</div>";
 echo "</div>";
 ?>
